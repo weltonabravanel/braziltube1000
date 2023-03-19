ef7073f850d53479894b5480b281e4fc7c86c384
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-18 21:05:28 -0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -32,7 +32,7 @@ export const categories = [
   { name: "Send feedback", icon: <RiFeedbackLine />, type: "menu" },
 ];
 
-const GOOGLE_API_KEY = "AIzaSyAkpSUobS1_L2FztkyQ_2YM1kHobjv09CQ,AIzaSyDo_0YDdRQ38vbVQ19Ng8bAHhRaNvyj_1s,AIzaSyAblinAxL4_M8EW2EZdeFVyiNV238ioFAY,AIzaSyDNZBDmYooRfOzZI31DvFh2GGrcoxuWNXw,AIzaSyDeEu_SyZRpCY52pZv5AMkNLVMSl-BY5pE,AIzaSyBM75GfcrEbkNqXDHJ99TtVwqtwMp9R62s,AIzaSyDNNC7tbd8qj0oXl2Qzo3SJpWFXCyuiznc,AIzaSyAMdROKeeHU7tf4tRN-IU8Sl5HLowoQgf4,AIzaSyC0u6BRsbIZt3Y-A1L1pFVpulI4jxx7TCs',
+const GOOGLE_API_KEY = "AIzaSyAkpSUobS1_L2FztkyQ_2YM1kHobjv09CQ',
 
 export const LIVE_CHAT_COUNT = 10;
 
